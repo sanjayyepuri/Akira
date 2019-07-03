12e21a1aab0bc60df35414f3d77951a52d19cf0e
Rushil Dandamudi <rushilcd@gmail.com>
2019-07-03 00:29:25 -0500

    test: added tests

---
+++ tests/add_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/sanjayyepuri/Akira/number"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	// t.Fatal("not implemented")
+	var num1 float64 = 5
+	var num2 float64 = -5
+
+	num3 := num1 + num2
+
+	if num3 != 0 {
+		t.Errorf("5 + -5 = %g; want 0", num3)
+	}
+
+	var command string = "5 - 5"
+	out, err := number.CalculateCommand(command)
+	if err != nil {
+		t.Errorf("add command failed with %s", err)
+	}
+
+	if out != 0 {
+		t.Errorf("5 - 5 = %g; want 0", out)
+	}
+
+}
